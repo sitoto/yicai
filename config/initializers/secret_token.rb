d0a543946a6800ba524d906ec3ea39151f8df795
eshow <76729530@qq.com>
2013-01-31 11:07:26 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yicai::Application.config.secret_token = 'a84c7996cf76945302934190feef1e911b5bb2f940d3b07204d709dca79afad2b186cc1a5daf0ef0473057439863f741065a35de1674a9f97547c402bed63a95'
